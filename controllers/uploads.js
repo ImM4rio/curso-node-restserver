02dd85c0f16f4211c332681955c378fd9d559250
Mario <mario.vazquez.otero@gmail.com>
2023-06-06 19:07:01 +0200

    uploads helper

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,31 +1,20 @@
 const { response } = require( "express" );
-const path = require( 'path' );
+const { subirArchivo } = require( "../helpers" );
 
-
-const cargarArchivo = ( req, res = response ) => {
+const cargarArchivo = async( req, res = response ) => {
   
-    if (!req.files || Object.keys(req.files).length === 0) {
+    if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
       res.status(400).json({ msg: 'No hay archivos que subir.' });
       return;
     }
-  
-    if ( !req.files.archivo ) {
-      res.status(400).json({ msg: 'No hay archivos que subir.' });
-      return;
-    }
-  
-    
-    const { archivo } = req.files;
-  
-    const uploadPath = path.join( __dirname, '../uploads/', archivo.name );
-  
-    archivo.mv(uploadPath, (err) => {
-      if (err) {
-        return res.status(500).json({ err });
-      }
-  
-      res.json({ msg: 'Archivo subido a ' + uploadPath });
+ 
+    // Imágenes
+    const nombre = await subirArchivo( req.files );
+
+    res.json({
+        nombre
     });
+
 }
 
 module.exports = {
